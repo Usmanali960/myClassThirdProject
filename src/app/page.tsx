fbe80d508f1824b895bc9518d51991431232d08f
Usmanali960 <wazeer9600@gmail.com>
2024-11-07 15:27:34 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,92 @@
-import Image from "next/image";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Sponsors from "./components/Sponsors";
+
+export type sectionData = {
+  topMain: boolean
+  mainBackground: boolean
+  textCenter: boolean
+  title: string
+  paragraph: string
+  button: string
+  rightSection: {
+    isBackground: boolean
+    isImage: boolean
+  }
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+  // My Hero section all data
+  const data: sectionData[] = [
+    {
+      topMain: false,
+      mainBackground: true,
+      textCenter: false,
+      title: "Get More Done with whitepace",
+      paragraph: "Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks",
+      button: "Try Whitepace free",
+      rightSection: { isBackground: true, isImage: false }
+    },
+    {
+      topMain: false,
+      mainBackground: false,
+      textCenter: false,
+      title: "Project Management",
+      paragraph: "Images, videos, PDFs and audio files are supported. Create math expressions and diagrams directly from the app. Take photos with the mobile app and save them to a note.",
+      button: "Get Started",
+      rightSection: { isBackground: true, isImage: false }
+    },
+    {
+      topMain: true,
+      mainBackground: false,
+      textCenter: false,
+      title: "Work\ntogeather",
+      paragraph: "With whitepace, share your notes with your colleagues and collaborate on them.You can also publish a note to the internet and share the URL with others.",
+      button: "Try it now",
+      rightSection: { isBackground: false, isImage: true }
+    },
+    {
+      topMain: false,
+      mainBackground: true,
+      textCenter: false,
+      title: "Get More Done with whitepace",
+      paragraph: "Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks",
+      button: "Try Whitepace free",
+      rightSection: { isBackground: true, isImage: false }
+    },
+    {
+      topMain: true,
+      mainBackground: false,
+      textCenter: false,
+      title: "Customise it\nto your needs",
+      paragraph: "Customise the app with plugins, custom themes and multiple text editors (Rich Text or Markdown). Or create your own scripts and plugins using the Extension API.",
+      button: "Let’s Go",
+      rightSection: { isBackground: true, isImage: false }
+    },
+    {
+      topMain: true,
+      mainBackground: true,
+      textCenter: true,
+      title: "Your work, everywhere you are",
+      paragraph: "Access your notes from your computer, phone or tablet by synchronising with various services, including whitepace, Dropbox and OneDrive. The app is available on Windows, macOS, Linux, Android and iOS. A terminal app is also available!",
+      button: "Try Taskey",
+      rightSection: { isBackground: false, isImage: false }
+    },
+  ]
+
+  return (
+    <div className="overflow-x-hidden">
+      <Navbar />
+      {/* // Here i run my map loo over the hero section data*/}
+      {data.map((item, index) => (
+        // here i can export the hero section data with the help of props
+        <Hero key={index} data={item} />
+      ))}
+      <Sponsors />
+      <Footer />
     </div>
   );
 }
